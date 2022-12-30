a6e9542f37751c6a8e9687c479761d165037f388
Jean-Denis Bertron <jbertron@spotx.tv>
2022-12-30 14:24:00 -0700

    Factored out modulus, but needs fix for claimReward t get the correct N.

--- contracts/BqETH.sol
+++ contracts/BqETH.sol
@@ -15,7 +15,7 @@ import './PietrzakVerifier.sol';
 /// @title BqETH Contract
 contract BqETH is PietrzakVerifier {
 
-  string public name = "BqETH Contract version 0.1";
+  string public name = "BqETH Contract version 0.2";
   bytes32 constant salt = "BqETH";
   address owner;  
   // TODO: store claimblock in the puzzle, so multiple puzzles can be claimed in the same block
@@ -31,16 +31,19 @@ contract BqETH is PietrzakVerifier {
         return name;
   }
 
+  struct Moduli {
+    bytes N;            // The modulus
+    string phi;         // The encrypted phi
+  }
+
   struct Puzzle {
     address creator;    // The user who registered the puzzle
     address payable farmer;     // Last farmer to claim
-    bytes N;            // The modulus
     bytes x;            // The start value
     uint256 t;          // The time parameter
+    uint256 reward;     // The amount that should be dispensed
     uint256 sdate;      // The start date or next pid in chain
     bytes32 h3;         // H3 Hash value of the solution
-    uint256 reward;     // The amount that should be dispensed
-    string phi;         // The encrypted phi
     uint256 head;       // Chain head pid
   }
 
@@ -62,6 +65,7 @@ contract BqETH is PietrzakVerifier {
   // instead of x1. The re-encryption policy can be linked to the last puzzle.
 
   // We're storing the creator in the puzzle, indexing puzzles by their hash
+  mapping(address => Moduli) public userModuli;
   mapping(uint256 => Puzzle) public userPuzzles;
   mapping(address => uint) public escrow_balances;
   mapping(address => ActivePolicy) public activePolicies;
@@ -105,7 +109,7 @@ contract BqETH is PietrzakVerifier {
   // Some unique key for each puzzle
   function puzzleKey(bytes memory _N, bytes memory _x, uint256 _t) public pure returns (uint256) 
   {
-    return uint(keccak256(abi.encodePacked(_N, _x, _t)));
+    return uint256(keccak256(abi.encodePacked(_N, _x, _t)));
   }
 
 struct ChainData {
@@ -127,6 +131,73 @@ struct PolicyData {
     string startTimestamp; 
     string endTimestamp;
 }
+
+  /// @notice Registers a user Puzzle Chain
+  /// @dev This registration creates an entry per puzzle in the userPuzzles map. The parameters completely and uniquely define the puzzle
+  /// and may be repeated across multiple puzzles since (N,φ) will be re-used until N has decayed. 
+  /// @param _N bytes  The prime composite modulus
+  /// @param _c[] ChainData  The puzzle initial challenges
+  /// @param _phi string  This is the encrypted value of phi, encrypted for the caller's public key
+  /// @param _sdate uint256  The start date (UTC) or the next puzzle hash in the chain
+  function recordPuzzles(
+    bytes memory _N, 
+    ChainData[] memory _c, 
+    string memory _phi, 
+    uint256 _sdate
+  ) internal returns (uint256)
+  {
+      uint256 reward_total = 0;
+      uint256 first_pid = 0;
+      uint256 head = puzzleKey(_N, _c[0].x, _c[0].t);
+      for(uint i = 0; i < _c.length; i++){
+
+        uint256 ph = puzzleKey(_N, _c[i].x, _c[i].t);
+        address payable _farmer;
+        // TODO Check that the puzzle did not already exist:
+        // Puzzle memory puzzle = userPuzzles[ph];
+        // require(puzzle.N != 0, "Puzzle already registered");   // We cannot afford a collision 
+
+        //Store the puzzle
+        Moduli memory mod;
+        mod.N = _N;
+        mod.phi = (i == 0 || i == _c.length-1)? _phi : '' ;  // Only first and last puzzle get encrypted phi
+        userModuli[msg.sender] = mod;
+
+        Puzzle memory pz;
+        pz.creator = msg.sender;
+        pz.farmer = _farmer;
+        
+        pz.x = _c[i].x;
+        pz.t = _c[i].t;
+        pz.sdate = (i == _c.length-1)? _sdate : _c[i].pid;  // Only last puzzle gets sdate
+        pz.h3 = _c[i].h3;
+        pz.reward = _c[i].reward;
+        
+        pz.head = head;
+
+        userPuzzles[ph] = pz;
+
+        reward_total += _c[i].reward;
+
+        if (i == 0) {
+          activePuzzles[msg.sender] = ph;
+          first_pid = ph;
+        }
+
+        // TODO msg.value better be more than the sum of puzzle chain rewards
+        require(msg.value >= reward_total);
+
+        console.log("Registered puzzle with Hash :'%s'", ph);
+        // Send the Event
+        emit NewPuzzleRegistered(msg.sender, ph, 
+                                (i==0) ? true:false);   // First puzzle in a chain is read-to-work
+      }
+      // Add to the escrow total for the creator's address.
+      escrow_balances[msg.sender] += msg.value;
+
+      return first_pid;
+  }
+
   /// @notice Registers a user Puzzle Chain
   /// @dev This registration creates an entry per puzzle in the userPuzzles map. The parameters completely and uniquely define the puzzle
   /// and may be repeated across multiple puzzles since (N,φ) will be re-used until N has decayed. 
@@ -147,48 +218,7 @@ struct PolicyData {
     ) public payable 
   returns (uint256) 
   {
-      uint256 ph = 0;
-      uint256 reward_total = 0;
-      uint256 first_pid = 0;
-      uint256 head = puzzleKey(_N, _c[0].x, _c[0].t);
-
-      for(uint i = 0; i < _c.length; i++){
-
-        ph = puzzleKey(_N, _c[i].x, _c[i].t);
-        address payable _farmer;
-        // TODO: Check that the puzzle did not already exist:
-
-        //Store the puzzle
-        Puzzle memory pz;
-        pz.creator = msg.sender;
-        pz.farmer = _farmer;
-        pz.N = _N;
-        pz.x = _c[i].x;
-        pz.t = _c[i].t;
-        pz.sdate = (i == _c.length-1)? _sdate : _c[i].pid;  // Only last puzzle gets sdate
-        pz.h3 = _c[i].h3;
-        pz.reward = _c[i].reward;
-        pz.phi = (i == 0 || i == _c.length-1)? _phi : '' ;  // Only first and last puzzle get encrypted phi
-        pz.head = head;
-
-        userPuzzles[ph] = pz;
-
-        reward_total += _c[i].reward;
-
-        if (i == 0) {
-          activePuzzles[msg.sender] = ph;
-          first_pid = ph;
-        }
-        console.log("Registered puzzle with Hash :'%s'", ph);
-        // Send the Event
-        emit NewPuzzleRegistered(msg.sender, ph,
-                                (i==0) ? true:false);   // First puzzle in a chain is read-to-work
-      }
-      // TODO msg.value better be more than the sum of puzzle chain rewards
-      // require(msg.value >= reward_total);
-
-      // Add to the escrow total for the creator's address.
-      escrow_balances[msg.sender] += msg.value;
+      uint256 first_pid = recordPuzzles(_N, _c, _phi, _sdate);
 
       activePolicies[msg.sender] = ActivePolicy( msg.sender, first_pid, _messageKit, 
           _policy.encryptedTreasureMap,
@@ -197,6 +227,7 @@ struct PolicyData {
           _policy.policyEncryptingKey
         );
 
+      // This event is needed to ensure the sponsor can fund policies on behalf of users
       emit NewPolicyRegistered(    
           _policy.id,
           _policy.label,
@@ -210,7 +241,7 @@ struct PolicyData {
 
       return first_pid;
   }
-
+  
   /// @notice Registers a flipped user Puzzle
   /// @dev This registration creates an entry per puzzle in the userPuzzles map. The parameters completely and uniquely define the puzzle
   /// and may be repeated across multiple puzzles since (N,φ) will be re-used until N has decayed. 
@@ -233,52 +264,12 @@ struct PolicyData {
       // Puzzle memory previous = userPuzzles[prev];
       // Puzzle flip restricted to creator of the previous puzzle
       require(msg.sender == userPuzzles[prev].creator);
-      uint256 reward_total = 0;
-      uint256 first_pid = 0;
-      uint256 head = puzzleKey(_N, _c[0].x, _c[0].t);
 
-      for(uint i = 0; i < _c.length; i++){
-
-        uint256 ph = puzzleKey(_N, _c[i].x, _c[i].t);
-        address payable _farmer;
-        // TODO Check that the puzzle did not already exist:
-        // Puzzle memory puzzle = userPuzzles[ph];
-        // require(puzzle.N != 0, "Puzzle already registered");   // We cannot afford a collision 
-
-        //Store the puzzle
-        Puzzle memory pz;
-        pz.creator = msg.sender;
-        pz.farmer = _farmer;
-        pz.N = _N;
-        pz.x = _c[i].x;
-        pz.t = _c[i].t;
-        pz.sdate = (i == _c.length-1)? _sdate : _c[i].pid;  // Only last puzzle gets sdate
-        pz.h3 = _c[i].h3;
-        pz.reward = _c[i].reward;
-        pz.phi = (i == 0 || i == _c.length-1)? _phi : '' ;  // Only first and last puzzle get encrypted phi
-        pz.head = head;
-
-        userPuzzles[ph] = pz;
-
-        reward_total += _c[i].reward;
-
-        if (i == 0) {
-          activePuzzles[msg.sender] = ph;
-          first_pid = ph;
-        }
-        console.log("Registered puzzle with Hash :'%s'", ph);
-        // Send the Event
-        emit NewPuzzleRegistered(msg.sender, ph, 
-                                (i==0) ? true:false);   // First puzzle in a chain is read-to-work
-      }
-      // TODO msg.value better be more than the sum of puzzle chain rewards
-      // require(msg.value >= reward_total);
-
-      // Add to the escrow total for the creator's address.
-      escrow_balances[msg.sender] += msg.value;
+      uint256 first_pid = recordPuzzles(_N, _c, _phi, _sdate);
 
       // Look up the active policy object and just change the treasuremap
       ActivePolicy storage policy = activePolicies[userPuzzles[prev].creator];
+
       // Only these 3 things change
       policy.pid = first_pid;
       policy.encryptedTreasureMap = _policy.encryptedTreasureMap;
@@ -295,7 +286,7 @@ struct PolicyData {
           _policy.startTimestamp,
           _policy.endTimestamp
       );
-      return first_pid;
+    return first_pid;
   }
 
   function getActiveChain(address _user) public view 
@@ -375,11 +366,12 @@ struct PolicyData {
       )
   {
       Puzzle memory puzzle = userPuzzles[_pid];
+      Moduli memory mod = userModuli[puzzle.creator];
       ActivePolicy memory policy = activePolicies[puzzle.creator];
 
       return (_pid,
               puzzle.creator,               // The puzzle creator
-              puzzle.N,                     // The modulus
+              mod.N,                        // The modulus
               puzzle.x,                     // The start value
               puzzle.t,                     // The time parameter
               puzzle.h3,                    // H3 Hash value of the solution
@@ -387,7 +379,7 @@ struct PolicyData {
               policy.aliceVerifyingKey,     // The verifying key
               policy.messageKit,            // The secret
               policy.encryptedTreasureMap,  // The associated NuCypher policy treasuremap
-              puzzle.phi,                   // The encrypted phi
+              mod.phi,                      // The encrypted phi
               puzzle.sdate                  // The start date or next puzzle pid
       );
   }
@@ -429,9 +421,10 @@ struct PolicyData {
       bytes32 x2 = sha256(abi.encodePacked(salt,_y));
       require(sha256(abi.encodePacked(x2,h1)) == puzzle.h3, "Solution must match commitment.");
 
+      Moduli memory mod = userModuli[puzzle.creator];
       // Now we can bother to verify
       uint256 d = log2(puzzle.t)-1;
-      if (verifyProof(puzzle.N, puzzle.x, d, _y, 0, _proof)) 
+      if (verifyProof(mod.N, puzzle.x, d, _y, 0, _proof)) 
       {
             puzzle.farmer.transfer(puzzle.reward);
             escrow_balances[puzzle.creator] -= puzzle.reward;
@@ -456,9 +449,11 @@ struct PolicyData {
                 pid_to_clear = next_pid;
               }
               while (pid_to_clear > Y3K);
-                // clear out the puzzle if no new one took its place
+                
+              // clear out the puzzle if no new one took its place
               if (activePuzzles[puzzle.creator] == chain_head) {
                 delete activePuzzles[puzzle.creator];
+                delete userModuli[puzzle.creator];
               }
             }
       }
